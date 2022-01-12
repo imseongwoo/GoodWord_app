331df72b053a11cd7b11d51adbeb48f2bcaf2bef
임성우 <seongwoorang@naver.com>
2022-01-12 16:54:56 +0900

    binding
    binding 사용 버튼 이벤트 처리

--- app/src/main/java/org/techtown/goodword/MainActivity.kt
+++ app/src/main/java/org/techtown/goodword/MainActivity.kt
@@ -1,11 +1,24 @@
 package org.techtown.goodword
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import org.techtown.goodword.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding.showAllSentenceBtn.setOnClickListener {
+
+            val intent = Intent(this,SentenceActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
