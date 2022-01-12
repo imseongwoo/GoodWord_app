0e2e65bc53ff8167b45cbf4e760035bff78e2cfb
임성우 <seongwoorang@naver.com>
2022-01-12 15:54:54 +0900

    명언앱

---
+++ app/src/main/java/org/techtown/goodword/SentenceActivity.kt
@@ -0,0 +1,11 @@
+package org.techtown.goodword
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SentenceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sentence)
+    }
+}
